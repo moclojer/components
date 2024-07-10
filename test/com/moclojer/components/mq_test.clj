5f976fb11585b9930f7f616a85950153da7e116c
J0sueTM <teodoro.josue@protonmail.ch>
2024-07-10 10:28:46 -0300

    refactor: switch to in-hous `clj-rq`

--- test/com/moclojer/components/mq_test.clj
+++ test/com/moclojer/components/mq_test.clj
@@ -15,7 +15,13 @@
 (defn fake-worker
   [message _]
   (is (= message {:event "test"}))
-  (swap! state (fn [_] message)))
+  (reset! state message))
+
+(def job-state (atom nil))
+
+(defn job-worker
+  [message _]
+  (swap! job-state conj message))
 
 (defn publish-message [msg channel]
   (flow
@@ -31,7 +37,9 @@
     :database (component/using (components/new-database) [:config])
     :sentry (component/using (components/new-sentry-mock) [:config])
     :mq (component/using (components/new-mq [{:handler fake-worker
-                                              :channel "test"}])
+                                              :channel "test"}
+                                             {:handler job-worker
+                                              :channel "test-job"}])
                          [:config :database :storage :sentry])
     :storage (component/using (components/new-storage) [:config]))))
 
@@ -39,12 +47,26 @@
   {:init (utils/start-system! create-and-start-system)
    :cleanup utils/stop-system!
    :fail-fast? true}
+  #_(flow
+      "should publish and consume a message"
+      (publish-message {:event "test"} "test")
+      (flow "sleeping and check the state"
+        (match? {:event "test"}
+                (state/return
+                 (utils/exec-with-timeout
+                  #(deref state)
+                  10000 500)))))
   (flow
-    "should publish and consume a message"
-    (publish-message {:event "test"} "test")
-    (flow "sleeping and check the state"
-      (match? {:event "test"}
-              (state/return
-               (utils/exec-with-timeout
-                #(deref state)
-                10000 500))))))
+    "should trigger a job 2 times within 5.5 seconds"
+    [:let [job {:channel "test-job"
+                :event {:hello :world}
+                :sleep 2500
+                :max-attempts 2}]
+     mq (state-flow.api/get-state :mq)]
+    (state/invoke #(mq/start-job! mq job))
+    (match?
+     (repeatedly 2 (constantly {:hello :world}))
+     (state-flow.api/return
+      (do
+        (Thread/sleep 5500)
+        @job-state)))))
