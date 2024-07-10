eecb2794d387f9a0bad9006be3b71f8431aa8e54
J0sueTM <teodoro.josue@protonmail.ch>
2024-07-10 10:28:46 -0300

    refactor: switch to in-hous `clj-rq`

--- src/com/moclojer/components/mq.clj
+++ src/com/moclojer/components/mq.clj
@@ -10,7 +10,16 @@
   (try-op! [this op-name args ctx]
     "Wraps our `clj-rq` operations in order for our components to call sentry
      if an error or exception occurs.")
-  (run-job! [this channel job ctx]))
+  (start-job! [this job]
+    "Starts a simple reoccuring `job` that triggers it.
+
+     A job should look like this:
+     `{:channel \"my-channel\"
+       :event {:hello 1}
+       :sleep 2500
+       :max-attempts 50}
+
+     Both `:sleep` and `:max-attempts` are optional."))
 
 (defn op-name->op-fn
   "Adapts given mq `operation name` to its relative function."
@@ -37,22 +46,30 @@
                                  :ex-message (.getMessage ex)})
                  (sentry/send-event! sentry {:throwable ex})))))))
 
-(defrecord MQ [config database storage http sentry workers jobs]
+(defrecord MQ [config database storage http sentry workers jobs blocking?]
   component/Lifecycle
   (start [this]
-    (logs/log :info "starting rq")
+    (logs/log :info "starting mq")
     (let [components {:config config
                       :database database
                       :storage storage
                       :http http
                       :sentry sentry}
-          client (rq/create-client (get-in config [:config :mq :uri]))]
+          client (rq/create-client (get-in config [:config :mq :uri]))
+          wrapped-workers (map #(wrap-worker % components sentry) workers)
+          subs (rq-pubsub/subscribe! client wrapped-workers :blocking? blocking?)]
+
+      (doseq [job jobs]
+        (start-job! (assoc this :client client) job))
+
       (merge this {:client client
-                   :subs (->> (map #(wrap-worker % components sentry) workers)
-                              (rq-pubsub/subscribe! client))
+                   :subs subs
+                   :jobs jobs
                    :components components})))
   (stop [this]
-    (rq/close-client (:client this)))
+    (logs/log :info "stopping mq")
+    (when-let [?client (:client this)]
+      (rq/close-client ?client)))
 
   IMQ
   (try-op! [this op-name args ctx]
@@ -67,7 +84,23 @@
                                    :ex-message (.getMessage ex)}))
         (sentry/send-event! (get-in this [:components :sentry])
                             {:throwable ex}))))
-  (run-job! [_this _channel _job _ctx]))
+  (start-job! [this job]
+    (let [{:keys [channel event sleep max-attempts]
+           :or {sleep 2000
+                ;; max-attempts of `0` means infinite attempts.
+                max-attempts 0}} job]
+      (logs/log :info "starting job" :ctx {:job job})
+      (future
+        (loop [attempt 1]
+          (let [infinite-attempts? (<= max-attempts 0)
+                attempt? (<= attempt max-attempts)]
+            (when (or infinite-attempts? attempt?)
+              (logs/log :info "running job"
+                        :ctx {:job job
+                              :attempt attempt})
+              (try-op! this :publish! [channel event] {})
+              (Thread/sleep sleep)
+              (recur (inc attempt)))))))))
 
 (defonce mock-channels (atom {}))
 
