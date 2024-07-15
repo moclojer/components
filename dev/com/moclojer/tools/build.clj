a1a7cdf846fc23bd4e7212c946dedfad44183d1e
J0sueTM <teodoro.josue@protonmail.ch>
2024-07-15 10:12:48 -0300

    chore: bump to `v0.0.7`

--- dev/com/moclojer/tools/build.clj
+++ dev/com/moclojer/tools/build.clj
@@ -4,7 +4,7 @@
    [clojure.string :as string]
    [clojure.tools.build.api :as b]))
 
-(def version "0.0.6")
+(def version "0.0.7")
 
 (def class-dir "target/classes")
 (def jar-file "target/com.moclojer.components.jar")
