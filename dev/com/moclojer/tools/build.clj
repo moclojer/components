7516dd224c9ad93d583a59f53831f0ce3d81c98b
j0suetm <me@j0suetm.com>
2024-10-22 17:57:56 -0300

    chore: bump v0.1.3

--- dev/com/moclojer/tools/build.clj
+++ dev/com/moclojer/tools/build.clj
@@ -4,7 +4,7 @@
    [clojure.string :as string]
    [clojure.tools.build.api :as b]))
 
-(def version "0.1.2")
+(def version "0.1.3")
 
 (def class-dir "target/classes")
 (def jar-file "target/com.moclojer.components.jar")
